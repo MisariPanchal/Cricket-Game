30dc8a038d7c17fe13af6d64eeef8ccd14edca4c
Misari <116897139+MisariPanchal@users.noreply.github.com>
2024-06-30 07:42:59 +0000

    Storing the score

--- index.js
+++ index.js
@@ -10,41 +10,59 @@ function randomChoice(){
         return 'Stump';
     }
 }
+
+let score = {
+    win:0,
+    lost:0,
+    tie:0
+};
+
 function result(userMove){
     let computerMove = randomChoice();
     let outcome;
     if(userMove === 'Bat'){
         if (computerMove == 'Bat') {
             outcome = `It's a tie`;
+            score.tie++;
         }
         else if (computerMove == 'Ball') {
             outcome = `You won !!!`;
+            score.win++;
         }
         else {
             outcome = `Computer Won !!!`;
+            score.lost++;
         }
     }
     else if(userMove === 'Ball'){
         if (computerMove == 'Ball') {
             outcome = `It's a tie`;
+            score.tie++;
         }
         else if (computerMove == 'Stump') {
             outcome = `You won !!!`;
+            score.win++;
         }
         else {
             outcome = `Computer Won !!!`;
+            score.lost++;
         }
     }
     else{
         if (computerMove == 'Stump') {
             outcome = `It's a tie`;
+            score.tie++;
         }
         else if (computerMove == 'Bat') {
             outcome = `You won !!!`;
+            score.win++;
         }
         else {
             outcome = `Computer Won !!!`;
+            score.lost++;
         }
     }
-    alert(`Your choice is ${userMove} and Computer choice is ${computerMove}. Therefore, ${outcome}`);
+    alert(`Your choice is ${userMove} and Computer choice is ${computerMove}.
+Therefore, ${outcome}
+Won: ${score.win}, Lost: ${score.lost}, Tie: ${score.tie}`);
 }
